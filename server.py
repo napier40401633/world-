de88d272fa0882e70cb86285eba52d8e4ab507b1
KMT <kyawmyotun999@gmail.com>
2018-04-06 08:26:41 +0630

    second commit

--- server.py
+++ server.py
@@ -54,8 +54,7 @@ def editCountryByNamePage(n):
 		for x in w:
 				if x['name'] == n:
 						c = x
-		return render_template('countryedit.html',
-													c=c)
+		return render_template('countryedit.html',c=c)
 
 @app.route('/updateCountryByName')
 def updateCountryByNamePage():
@@ -66,6 +65,10 @@ def updateCountryByNamePage():
 						c = x
 		c['capital'] = request.args.get('capital')
 		c['continent'] = request.args.get('continent')
+		c['tld'] = request.args.get('tld')
+		c['area'] = int(request.args.get('area'))
+		c['population']  = int(request.args.get('population'))
+		c['gdp']  = float(request.args.get('gdp'))
 		return render_template(
 					'country.html',
 					c = c)
@@ -83,8 +86,29 @@ def deleteCountry(n):
                 page_number = 0,
                 page_size = page_size
                 )
+@app.route('/createcountry')
+def createCountryByName():
+	return render_template('createcountry.html', c=c)
 
-app.run(host='0.0.0.0', port=5033, debug=True)
+'''@app.route('/addcountrybyname')
+def addcountryByNamePage():
+	n = request.args.get('country')
+	c = {}
+	c['name'] = n
+	c['capital'] = request.args.get('capital')
+	c['continent'] = request.args.get('continent')
+	c['area'] = int(request.args.get('area'))
+	c['gdp']  = float(request.args.get('gdp'))
+	c['tld']  = request.args.get('tld')
+	c['population']  = int(request.args.get('population'))
+	w.append(c)
+	w.sort(key = lambda c: c['name'])
+	return render_template(
+		'country.html',
+		c = c)'''
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5033, debug=True)
 
 
 
